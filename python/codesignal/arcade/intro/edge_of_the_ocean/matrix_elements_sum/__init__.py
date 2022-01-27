9ac7b722fda04851ca08b0b1bfc921225238c0a8
Da CodeKid <damacusr@gmail.com>
2022-01-27 17:23:27 -0500

    Added solution for matrixElementsSum: (CodeSignal)

---
+++ python/codesignal/arcade/intro/edge_of_the_ocean/matrix_elements_sum/__init__.py
@@ -0,0 +1,16 @@
+def solution(matrix):
+    rent = 0
+    for section in range(len(matrix[0])):
+        for room in range(len(matrix)):
+            if matrix[room][section] == 0:
+                # ghost found, exit the entire section below
+                break
+            rent += matrix[room][section]
+    return rent
+
+
+print(solution([
+    [0, 1, 1, 2],
+    [0, 5, 0, 0],
+    [2, 0, 3, 3]
+]))
